2584b377c01cae932681cc065c688247d9ada240
Kuba Sobecki <gosustyle@gmail.com>
2022-12-05 13:29:24 +0100

    Implement some basic routes and redirects with React Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { Route, Switch, Redirect } from 'react-router-dom';
+import MainHeader from './components/MainHeader';
+import Welcome from './pages/Welcome';
+import Products from './pages/Products';
+import ProductDetails from './pages/ProductDetails';
+
 function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
+    return (
+        <div>
+            <MainHeader />
+            <main>
+                <Switch>
+                    <Route path="/" exact>
+                        <Redirect to="/welcome" />
+                    </Route>
+                    <Route path="/welcome">
+                        <Welcome />
+                    </Route>
+                    <Route path="/products" exact>
+                        <Products />
+                    </Route>
+                    <Route path="/products/:productId">
+                        <ProductDetails />
+                    </Route>
+                </Switch>
+            </main>
+        </div>
+    );
 }
 
 export default App;
