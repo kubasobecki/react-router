f6d78ce1775d236ddaa1dbe0a50cfa0c54bc222c
Kuba Sobecki <gosustyle@gmail.com>
2022-12-05 17:03:42 +0100

    Implement programmatic navigation on adding new quote

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 import QuoteForm from '../components/quotes/QuoteForm';
 
 const NewQuote = () => {
+    const history = useHistory();
+
     const addQuoteHandler = quoteData => {
         const { author, text } = quoteData;
         console.log(author, text);
+
+        history.push('/quotes');
     };
 
     return <QuoteForm onAddQuote={addQuoteHandler} />;
