e7335c3a5c6894436b4f1f53dcd1e85cb9356e57
Kuba Sobecki <gosustyle@gmail.com>
2022-12-05 17:45:55 +0100

    Add quote sorting with useHistory and useLocation hooks

--- src/components/quotes/QuoteList.js
+++ src/components/quotes/QuoteList.js
@@ -1,23 +1,55 @@
 import { Fragment } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import QuoteItem from './QuoteItem';
 import classes from './QuoteList.module.css';
 
-const QuoteList = (props) => {
-  return (
-    <Fragment>
-      <ul className={classes.list}>
-        {props.quotes.map((quote) => (
-          <QuoteItem
-            key={quote.id}
-            id={quote.id}
-            author={quote.author}
-            text={quote.text}
-          />
-        ))}
-      </ul>
-    </Fragment>
-  );
+const sortQuotes = (quotes, ascending) => {
+    return quotes.sort((quoteA, quoteB) => {
+        if (ascending) {
+            return quoteA.id > quoteB.id ? 1 : -1;
+        } else {
+            return quoteA.id < quoteB.id ? 1 : -1;
+        }
+    });
+};
+
+const QuoteList = props => {
+    const history = useHistory();
+    const location = useLocation();
+
+    const queryParams = new URLSearchParams(location.search);
+    const isSortingAscending = queryParams.get('sort') === 'asc';
+
+    const changeSortingHandler = () => {
+        history.push(`/quotes?sort=${isSortingAscending ? 'desc' : 'asc'}`);
+    };
+
+    const sortButtonLabel = `Sort ${
+        isSortingAscending ? ' Descending' : 'Ascending'
+    }`;
+
+    const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
+
+    return (
+        <Fragment>
+            <div className={classes.sorting}>
+                <button onClick={changeSortingHandler}>
+                    {sortButtonLabel}
+                </button>
+            </div>
+            <ul className={classes.list}>
+                {sortedQuotes.map(quote => (
+                    <QuoteItem
+                        key={quote.id}
+                        id={quote.id}
+                        author={quote.author}
+                        text={quote.text}
+                    />
+                ))}
+            </ul>
+        </Fragment>
+    );
 };
 
 export default QuoteList;
