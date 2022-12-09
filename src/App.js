970d3787a5f78d9b5d9073533cd38322bd9bedb1
Kuba Sobecki <gosustyle@gmail.com>
2022-12-09 09:08:49 +0100

    Convert to React Router v6

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
@@ -6,24 +6,21 @@ import ProductDetail from './pages/ProductDetail';
 import MainHeader from './components/MainHeader';
 
 function App() {
-  return (
-    <div>
-      <MainHeader />
-      <main>
-        <Switch>
-          <Route path='/welcome'>
-            <Welcome />
-          </Route>
-          <Route path='/products' exact>
-            <Products />
-          </Route>
-          <Route path='/products/:productId'>
-            <ProductDetail />
-          </Route>
-        </Switch>
-      </main>
-    </div>
-  );
+    return (
+        <div>
+            <MainHeader />
+            <main>
+                <Routes>
+                    <Route path="/welcome" element={<Welcome />} />
+                    <Route path="/products/*" element={<Products />} />
+                    <Route
+                        path="/products/:productId"
+                        element={<ProductDetail />}
+                    />
+                </Routes>
+            </main>
+        </div>
+    );
 }
 
 export default App;
