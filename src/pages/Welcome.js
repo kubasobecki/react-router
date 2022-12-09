efe4b37dd3916aa6993463bcca43f5011b17e683
Kuba Sobecki <gosustyle@gmail.com>
2022-12-09 09:47:25 +0100

    Convert to Router v6

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,14 +1,13 @@
-import { Route } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const Welcome = () => {
-  return (
-    <section>
-      <h1>The Welcome Page</h1>
-      <Route path="/welcome/new-user">
-        <p>Welcome, new user!</p>
-      </Route>
-    </section>
-  );
+    return (
+        <section>
+            <h1>The Welcome Page</h1>
+            <Link to={'new-user'}>New User</Link>
+            <Outlet />
+        </section>
+    );
 };
 
 export default Welcome;
