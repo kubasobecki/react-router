b976220f365d1e6c8f8681922ba184623d1ec89f
Kuba Sobecki <gosustyle@gmail.com>
2022-12-09 09:11:11 +0100

    Initial files for example 2 router 5

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
 import { Route, Switch, Redirect } from 'react-router-dom';
-import MainHeader from './components/MainHeader';
+
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
-import ProductDetails from './pages/ProductDetails';
+import ProductDetail from './pages/ProductDetail';
+import MainHeader from './components/MainHeader';
 
 function App() {
-    return (
-        <div>
-            <MainHeader />
-            <main>
-                <Switch>
-                    <Route path="/" exact>
-                        <Redirect to="/welcome" />
-                    </Route>
-                    <Route path="/welcome">
-                        <Welcome />
-                    </Route>
-                    <Route path="/products" exact>
-                        <Products />
-                    </Route>
-                    <Route path="/products/:productId">
-                        <ProductDetails />
-                    </Route>
-                </Switch>
-            </main>
-        </div>
-    );
+  return (
+    <div>
+      <MainHeader />
+      <main>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/welcome' />
+          </Route>
+          <Route path='/welcome'>
+            <Welcome />
+          </Route>
+          <Route path='/products' exact>
+            <Products />
+          </Route>
+          <Route path='/products/:productId'>
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
 }
 
 export default App;
+
+// our-domain.com/welcome => Welcome Component
+// our-domain.com/products => Products Component
+// our-domain.com/product-detail/a-book
