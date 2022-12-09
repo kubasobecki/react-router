e09da604cadb37a6de1634f6261804f6229b3314
Kuba Sobecki <gosustyle@gmail.com>
2022-12-09 08:34:43 +0100

    Initial files for example 1 router v5

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import { Route, Switch, Redirect } from 'react-router-dom';
-import MainHeader from './components/MainHeader';
+import { Route, Switch } from 'react-router-dom';
+
 import Welcome from './pages/Welcome';
 import Products from './pages/Products';
-import ProductDetails from './pages/ProductDetails';
+import ProductDetail from './pages/ProductDetail';
+import MainHeader from './components/MainHeader';
 
 function App() {
-    return (
-        <div>
-            <MainHeader />
-            <main>
-                <Switch>
-                    <Route path="/" exact>
-                        <Redirect to="/welcome" />
-                    </Route>
-                    <Route path="/welcome">
-                        <Welcome />
-                    </Route>
-                    <Route path="/products" exact>
-                        <Products />
-                    </Route>
-                    <Route path="/products/:productId">
-                        <ProductDetails />
-                    </Route>
-                </Switch>
-            </main>
-        </div>
-    );
+  return (
+    <div>
+      <MainHeader />
+      <main>
+        <Switch>
+          <Route path='/welcome'>
+            <Welcome />
+          </Route>
+          <Route path='/products' exact>
+            <Products />
+          </Route>
+          <Route path='/products/:productId'>
+            <ProductDetail />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
 }
 
 export default App;
+
+// our-domain.com/welcome => Welcome Component
+// our-domain.com/products => Products Component
+// our-domain.com/product-detail/a-book
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,19 +1,5 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
-
 const Welcome = () => {
-    const params = useParams();
-    console.log(params);
-
-    return (
-        <section>
-            <h1>The Welcome Page</h1>
-            <Route path="/welcome/new-user">
-                <p>Hello user</p>
-            </Route>
-        </section>
-    );
+  return <h1>The Welcome Page</h1>;
 };
 
 export default Welcome;
