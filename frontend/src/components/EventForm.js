b66362262ecbb37a37c2ef604feeb58d1b69ef2d
Kuba Sobecki <gosustyle@gmail.com>
2023-02-12 21:12:40 +0100

    Add UI feedback for submitting new event

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { Form, useNavigate } from 'react-router-dom';
+import { Form, useNavigate, useNavigation } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
     const navigate = useNavigate();
+
+    const navigation = useNavigation();
+
+    const isSubmitting = navigation.state === 'submitting';
+
     function cancelHandler() {
         navigate('..');
     }
@@ -52,10 +57,16 @@ function EventForm({ method, event }) {
                 />
             </p>
             <div className={classes.actions}>
-                <button type="button" onClick={cancelHandler}>
-                    Cancel
+                <button
+                    type="button"
+                    onClick={cancelHandler}
+                    disabled={isSubmitting}
+                >
+                    {isSubmitting ? 'Submitting' : 'Cancel'}
                 </button>
-                <button>Save</button>
+                <button disabled={isSubmitting}>
+                    {isSubmitting ? 'Submitting' : 'Save'}
+                </button>
             </div>
         </Form>
     );
